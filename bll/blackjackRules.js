3eda97f4cf4792a32cc54b1d2105644886ecc648
quivsoth <phillip.knezevich@gmail.com>
2020-08-16 23:47:01 -0400

    Fixed player values

--- bll/blackjackRules.js
+++ bll/blackjackRules.js
@@ -4,21 +4,9 @@ const fs = require("fs");
 const MultiShuffle = require("../shuffle");
 const { pid } = require("process");
 
-const blackjackRules = () => {};
-const isNanOrZero = n => isNaN(n) ? 0 : n
-
-try {
+const blackjackRules = () => {
 	const rules = yaml.safeLoad(fs.readFileSync("bll/blackjackRules.yml", "utf8"));
-
-	// Deck Shuffle
-	const deck = MultiShuffle();
-
-
-	let playerCards = [ deck.pop(), deck.pop()];
-	let dealerCards = [ deck.pop(), deck.pop()];
-
-	// const supervariable = n => n == 10 ? n : (n--, console.log(n), supervariable(n))
-
+	const isNanOrZero = n => isNaN(n) ? 0 : n
 	const aceCheck = (hand) => {
 		let aces = hand.filter(function (cards) { return cards.cardFace == 'Ace'});
 		let noAces = hand.filter(function (cards) { return cards.cardFace != 'Ace'});
@@ -35,37 +23,36 @@ try {
 			});
 		}
 	}
+	// const dealerRun = (dealerCards) => {
+	// 	const hand = {
+	// 		hand: dealerCards,
+	// 		result: 0
+	// 	}
 
-	const dealerRun = (dealerCards) => {
-		const hand = {
-			hand: dealerCards,
-			result: 0
-		}
-		aceCheck(dealerCards);
-		hand.result = dealerCards.map(item => item.cardValue).reduce((a, b) => a + b);
+	// 	aceCheck(dealerCards);
+	// 	hand.result = dealerCards.map(item => item.cardValue).reduce((a, b) => a + b);
 
-		if (hand.result > 21) return dealerCards;
-		if (hand.result < 17) {
-			dealerCards.push(deck.pop());
-			dealerRun(dealerCards);
-		}
-		return hand;
-	}
+	// 	if (hand.result > 21) return dealerCards;
+	// 	if (hand.result < 17) {
+	// 		dealerCards.push(deck.pop());
+	// 		dealerRun(dealerCards);
+	// 	}
+	// 	return hand;
+	// }
+	const RunPlayer = (playerCards, dealerCard) => {
+		const result = { tally: 0, hand: [] }
+		const cardTally = playerCards[0].map(item => item.cardValue).reduce((a, b) => isNanOrZero(a) + isNanOrZero(b));
 
-	const FirstPlay = (playerCards, dealerCard) => {
-		const cardTally = playerCards.map(item => item.cardValue).reduce((a, b) => isNanOrZero(a) + isNanOrZero(b));
-
-		if (playerCards.length == 2) {
-			playerCards.sort(function(a,b){ return ((+b.cardValue==b.cardValue) && (+a.cardValue != a.cardValue)) || (a.cardValue - b.cardValue) }).reverse();
-			const handCode = playerCards[0].cardValue + "," + playerCards[1].cardValue;
-			console.log("handCode : " + handCode);
-			console.log("dealerCard : " + dealerCard);
+		if (playerCards[0].length == 2) {
+			const sorted = playerCards[0].sort(function(a,b){ return ((+b.cardValue==b.cardValue) && (+a.cardValue != a.cardValue)) || (a.cardValue - b.cardValue) }).reverse();
+			const handCode = sorted[0].cardValue + "," + sorted[1].cardValue;
 
 			// 1. check for blackjack
-			if(handCode == 'A,10') {console.log("BLACKJACK!!");}
+			if(handCode == 'A,10') { result.tally = "BJ" }
 
 			//Eval the handcode to see the options (all options will have an array)
-			let hitOption = rules.filter(function (cards) { return cards.Hand.PlayerTotal == (handCode.includes('A') || (playerCards[0].cardValue == playerCards[1].cardValue) ? handCode : cardTally); });
+			let hitOption = rules.filter(function (cards) { return cards.Hand.PlayerTotal == (handCode.includes('A') || (sorted[0].cardValue == sorted[1].cardValue) ? handCode : cardTally); });
+
 
 			//2. hit,split or double?
 			const shouldDouble = hitOption[0].Hand.Double.indexOf(dealerCard);
@@ -80,10 +67,12 @@ try {
 				//TODO we are here
 			}
 
-			if (shouldDouble == -1 && shouldHit == -1 && shouldSplit == -1) { console.log("Action: Stay");}
+			if (shouldDouble == -1 && shouldHit == -1 && shouldSplit == -1) { console.log("Action: Stay"); }
 		}
+		result.tally = playerCards[0].map(item => item.cardValue).reduce((a, b) => a + b);
+		result.hand.push(playerCards[0]);
+		return result;
 	}
-
 	const AutoRunOut = (playerCards, dealerCard) => {
 		console.log("dealerCard: " + dealerCard);
 		aceCheck(playerCards);
@@ -108,58 +97,52 @@ try {
 			console.log("Action: Hit");
 			playerCards.push(deck.pop());
 			AutoRunOut(playerCards, dealerCard);
-		 }
+		}
 		return playerCards;
 	};
 
 
-
-	const aceTest = () => {
-		let card1 = {
-			id: 2,
-			suit: "Hearts",
-			cardFace: "Ace",
-			cardValue: "A"
-		}
-		let card2 = {
-			id: 12,
-			suit: "Spades",
-			cardFace: "Three",
-			cardValue: 3
-		}
-		let card3 = {
-			id: 24,
-			suit: "Hearts",
-			cardFace: "Two",
-			cardValue: 2
-		}
-		let card4 = {
-			id: 22,
-			suit: "Hearts",
-			cardFace: "Ace",
-			cardValue: "A"
-		}
-		let card5 = {
-			id: 92,
-			suit: "Clubs",
-			cardFace: "Ace",
-			cardValue: "A"
-		}
-
-		var p1 = [];
-		p1.push(deck.pop(), deck.pop());
-		// p1.push(card1, card2, card3, card4);
+	try {
+		//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 
 
+		// Deck Shuffle
+		const deck = MultiShuffle();
+
+		var playerCards = [ deck.pop(), deck.pop()];
+		var dealerCards = [ deck.pop(), deck.pop()];
+
+		var Table = {
+			players: [],
+			shoeResult: [],
+			dealer: []
+		}
+
+		var player1 = {
+			name: "Ximeng Liu",
+			money: 0,
+			hand: []
+		}
+
+		// Add player to the table
+		Table.players.push(player1);
+
+		// Deal first hand to the player
+		player1.hand.push(playerCards);
+
+		// Deal to the dealer
+		Table.dealer.push(dealerCards);
+
+		// Eval
+		var result = RunPlayer(Table.players[0].hand, dealerCards[0].cardValue);
+		// console.log(result.tally);
+		// console.log(result.hand);
+		//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+	} catch (e) {
+		console.log(e);
 	}
-
-	aceTest();
-	// const gg = AutoRunOut(playerHand, dealerHand[0].cardValue);
-	//const gg = dealerRun(dealerHand);
-	// console.log(gg);
-	//FirstPlay(playerHand, dealerHand[0].cardValue);
-
-} catch (e) {
-	console.log(e);
 }
+
+blackjackRules();
 module.exports = blackjackRules;
